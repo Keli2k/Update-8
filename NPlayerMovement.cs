b871d6a1f176560cd12bf6f3b03acba62ec422c7
Dylan Tuck <45218496+Keli2k@users.noreply.github.com>
2019-04-25 21:04:47 -0400

    Add files via upload

---
+++ NPlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 30.0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+
+    public void OnLanding()
+    {
+
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
